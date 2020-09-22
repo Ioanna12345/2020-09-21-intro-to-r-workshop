8b5ab1e0a889f58e6c2e99216c826fcbc8853f8e
Workshop User <noreply@workshop.com>
2020-09-22 00:07:25 +0000

    changes before 2nd break in epis002

--- Workshop/scripts/ep02-starting-with-data.R
+++ Workshop/scripts/ep02-starting-with-data.R
@@ -19,7 +19,9 @@ download.file(url = "https://ndownloader.figshare.com/files/2292169",
 surveys <- read.csv("data_raw/portal_data_joined.csv")
 
 # and take a look at it
-
+surveys
+head (surveys)
+View (surveys)
 
 
 # BTW, we assumed our data was comma separated, however this might not
@@ -29,11 +31,11 @@ surveys <- read.csv("data_raw/portal_data_joined.csv")
 
 # So what kind of an R object is "surveys" ?
 
-
+class(surveys)
 
 # ok - so what are dataframes ?
 
-
+str (surveys)
 
 # --------
 # Exercise
@@ -42,7 +44,9 @@ surveys <- read.csv("data_raw/portal_data_joined.csv")
 # What is the class of the object surveys?
 #
 # Answer:
-
+names (surveys)
+rownames (surveys)
+summary (surveys)
 
 # How many rows and how many columns are in this survey ?
 #
@@ -52,53 +56,70 @@ surveys <- read.csv("data_raw/portal_data_joined.csv")
 #
 #
 # Answer:
+42.67
 
 # Are there more Birds than Rodents ?
 #
 #
 # Answer:
 
+No
 
 # 
 # Topic: Sub-setting
-#
+
+
 
 # first element in the first column of the data frame (as a vector)
-
-
+surveys[1,1]
 # first element in the 6th column (as a vector)
 
-
+surveys [1,6]
 # first column of the data frame (as a vector)
 
+surveys[ ,6]
 
 # first column of the data frame (as a data frame)
 
-
+head (surveys[1])
+head (surveys[, 1])
 # first row (as a data frame)
 
+surveys [1, ]
 
 # first three elements in the 7th column (as a vector)
-
+surveys[1:3, 7]
 
 # the 3rd row of the data frame (as a data.frame)
 
-
-# equivalent to head(metadata)
-
+surveys[3, ]
+# equivalent to head(surveys)
+head(surveys)
+surveys[1:6, ]
 
 # looking at the 1:6 more closely
+1:6
+5:10
 
+
+surveys[ c(1,2,3,4,5,6),  ]
+surveys [ c(2,4,6), ]
 
 # we also use other objects to specify the range
 
+rows <- 6
+surveys [1:rows, 3]
 
 
-#
 # Challenge: Using slicing, see if you can produce the same result as:
-#
-#   tail(surveys)
-#
+
+#tail(surveys)
+end <- nrow(surveys)
+surveys [34781: nrow(surveys), ]
+surveys [(end-5): end, ]
+
+length (surveys[, 1])
+
 # i.e., print just last 6 rows of the surveys dataframe
 #
 # Solution:
@@ -107,25 +128,51 @@ surveys <- read.csv("data_raw/portal_data_joined.csv")
 
 # We can omit (leave out) columns using '-'
 
-
-
+surveys[-1]
+head (surveys[c( -2, -3, -4, -5)] )
+head (surveys [-  (1:3)] )
+head (surveys [-  (1:3)] )
 # column "names" can be used in place of the column numbers
 
+head (surveys ["month"] )
 
 
 #
 # Topic: Factors (for categorical data)
-#
 
+# eg: ranking: high medium low
+# Likert scales: very likey, neutral, unlikely, etc
+# Country: Australia, New Zealand
+
+gender <- c("male", "male", "female")
+gender <- factor( c("male", "male", "female") )
+gender 
+class (gender)
+levels (gender)
+nlevels(gender)
 
 # factors have an order
-
+temperature <- factor ( c("hot", "cold", "hot", "warm"))
+temperature [1]
+temperature [2]
+temperature
+levels (temperature)
+temperature <- factor ( c("hot", "cold", "hot", "warm"),
+                    levels = c( "cold", "warm", "hot") )
+levels (temperature)
 
 # Converting factors
-
+as.numeric(temperature)
+as.character(temperature)
 
 # can be tricky if the levels are numbers
 
+year <- factor (c(1990, 1983, 1977, 1998, 1990) )
+year
+as.numeric (year)
+as.character (year)
+
+as.numeric (as.character (year) )
 
 # so does our survey data have any factors
 
